3b89cea24081fff0ff7a9750625611ae6d63ed5f
Abel Meseret <abelmeseret@Abels-MacBook-Pro.local>
2024-07-26 05:36:05 +0300

    Set up routing with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
+import Index from './pages/Index'
+import Show from './pages/Show'
+import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +13,11 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/index" element={<Index />} />
+        <Route path="/show/:id" element={<Show />} />
+      </Routes>
     </div>
   )
 }
