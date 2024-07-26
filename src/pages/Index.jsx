da7461592901a20382370e2f7184512f5c7e43e1
Abel Meseret <abelmeseret@Abels-MacBook-Pro.local>
2024-07-26 07:54:25 +0300

    Add data fetching to Index component

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 
 function Index(props) {
+    const [itineraries, setItineraries] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/itineraries')
+          .then(response => setItineraries(response.data))
+          .catch(error => console.error('Error fetching itineraries:', error));
+      }, []);
 
     return (  
        <div>
          <h1>Itineraries</h1>
-         <p>Itineraries list</p>
+         <ul>
+        {itineraries.map(itinerary => (
+          <li key={itinerary._id}>
+            <Link to={`/show/${itinerary._id}`}>{itinerary.destination}</Link>
+          </li>
+        ))}
+      </ul>
+         
        </div>
 
     );
