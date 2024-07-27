c8b6ab68f8d518133ca5fb61f5306f6eee8c3648
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-07-27 12:15:35 -0400

    sign up form component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import Home from './components/Home'
 import Index from './pages/Index'
 import Show from './pages/Show'
+import Register from './components/Register'
 import './App.css'
 
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/index" element={<Index />} />
         <Route path="/show/:id" element={<Show />} />
+        <Route path="/register" element={<Register />}/>
       </Routes>
     </div>
   )
