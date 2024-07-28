35aeb5b04f7a4f06e20d95b88490622f56b8e7ed
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-07-28 17:12:17 -0400

    user can login successfully

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,66 @@
 import React from "react";
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "../App.css"
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../App.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const configuration = {
+      method: "post",
+      url: "https://mytinerary-backend-5vu5.onrender.com/user/login",
+      data: {
+        email,
+        password,
+      },
+    };
+    axios(configuration)
+      .then((result) => {
+        setLogin(true);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+  };
+
+  React.useEffect(() => {
+    if (login) {
+      navigate("/");
+    }
+  }, [login, navigate]);
+
   return (
     <div className="userForm">
       <h3>Log in</h3>
-      <Form>
-
+      <Form onSubmit={(e) => handleSubmit(e)}>
         <Form.Group controlId="formBasicEmail" className="formLabel">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter your email" />
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword" className="formLabel">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter your password" />
+          <Form.Control
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
+          />
         </Form.Group>
 
         <Button variant="primary" type="submit" className="userButton">
