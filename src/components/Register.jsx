ac87b5dc6c6b1ec1f53d164e11b767b7775052e4
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-07-28 16:56:40 -0400

    registration form linked with backend, user can create an account

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,34 +1,69 @@
 import React from "react";
+import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import "../App.css"
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "../App.css";
 
 function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [register, setRegister] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const configuration = {
+      method: "post",
+      url: "https://mytinerary-backend-5vu5.onrender.com/user/register",
+      data: {
+        email,
+        password,
+      },
+    };
+    axios(configuration)
+      .then((result) => {
+        setRegister(true);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+  };
+
+  React.useEffect(() => {
+    if (register) {
+      navigate("/login");
+    }
+  }, [register, navigate]);
+
   return (
     <div className="userForm">
       <h3>Sign Up</h3>
-      <Form>
-        <Form.Group controlId="formBasicFirstName" className="formLabel">
-          <Form.Label >First Name</Form.Label >
-          <Form.Control type="text" placeholder="First name" />
-        </Form.Group>
-
-        <Form.Group controlId="formBasicLastName" className="formLabel">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control type="text" placeholder="Last name" />
-        </Form.Group>
-
+      <Form onSubmit={(e) => handleSubmit(e)}>
         <Form.Group controlId="formBasicEmail" className="formLabel">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+          />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword" className="formLabel">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter password" />
+          <Form.Control
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter password"
+          />
         </Form.Group>
 
-        <Button variant="primary" type="submit" className="signUpButton">
+        <Button variant="primary" type="submit" className="userButton">
           Sign Up
         </Button>
       </Form>
