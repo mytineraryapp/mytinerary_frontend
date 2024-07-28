87eea79e18c0e6a37c49d131417096970ed47eb7
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-07-28 15:46:02 -0400

    updated registration form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,30 +4,30 @@ import "../App.css"
 
 function Register() {
   return (
-    <div>
+    <div className="register">
       <h3>Sign Up</h3>
       <Form>
-        {/* <Form.Group controlId="formBasicFirstName">
-          <Form.Label>First Name</Form.Label>
+        <Form.Group controlId="formBasicFirstName" className="formLabel">
+          <Form.Label >First Name</Form.Label >
           <Form.Control type="text" placeholder="First name" />
         </Form.Group>
 
-        <Form.Group controlId="formBasicLastName">
+        <Form.Group controlId="formBasicLastName" className="formLabel">
           <Form.Label>Last Name</Form.Label>
           <Form.Control type="text" placeholder="Last name" />
-        </Form.Group> */}
+        </Form.Group>
 
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group controlId="formBasicEmail" className="formLabel">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
         </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
+        <Form.Group controlId="formBasicPassword" className="formLabel">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control type="password" placeholder="Enter password" />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" className="signUpButton">
           Sign Up
         </Button>
       </Form>
