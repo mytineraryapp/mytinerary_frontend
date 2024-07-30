7a04a2e42396ebdb6593d640cac771558ce278de
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-07-29 20:25:11 -0400

    added loading/error state control to Trips component

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,18 +1,19 @@
-import { useParams } from "react-router-dom"
-
+import {useParams} from "react-router-dom"
 
 const Show = (props) => {
-    const params = useParams()
-    const id = params.id;
-    const trips = props.trips;
-    const trip = trips.find((t) => t._id === id)
+  const params = useParams()
+  const id = params.id;
+  const trips = props.trips;
+  const trip = trips.find((trip) => trip._id === id);
 
-    return (
-        <div className="trip">
-            <h1>{trip.destination}</h1>
-            <h4>{trip.startMonth}/{trip.startDay} - {trip.endMonth}/{trip.endDay}</h4>
-        </div>
-    )
-}
+  return (
+    <div className="trip">
+      <h1>{trip.destination}</h1>
+      <h4>
+        {trip.startMonth}/{trip.startDay} - {trip.endMonth}/{trip.endDay}
+      </h4>
+    </div>
+  );
+};
 
-export default Show
+export default Show;
