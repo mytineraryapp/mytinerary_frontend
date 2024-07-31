7a056ca65b5ac5e4b374fc628c689e727fddd194
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-07-31 18:16:55 -0400

    reverted route change

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/trips" element={<Trips />} />
+        <Route path="/trips/*" element={<Trips />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
