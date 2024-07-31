88b1f487d2dc41dbe3a9b1712ace08a03b4a5274
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-07-31 13:48:30 -0400

    footer with social links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
+import { Form, Button } from "react-bootstrap";
+import Footer from "./Footer"; 
 import "../App.css";
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h5>Start your next adventure</h5>
+      <Form.Control size="sm" type="text" placeholder="Where are you going?" />
+      <Button variant="primary" type="submit" className="go">
+        Go
+      </Button>
+      <Footer />
     </div>
   );
 }
